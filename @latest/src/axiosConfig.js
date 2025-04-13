c433dc699fa8246609370829e41969e276cd7d7c
Paraga2003 <153626230+Paraga2003@users.noreply.github.com>
2025-04-13 23:10:38 +0530

    Update axiosConfig.js

--- @latest/src/axiosConfig.js
+++ @latest/src/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'https://task-mate-project.onrender.com/',
+  baseURL: 'http://localhost:4000',
   headers: {
     'Content-Type': 'application/json',
   },
